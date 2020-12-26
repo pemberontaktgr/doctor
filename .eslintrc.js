4591b7e417f3cdf3ee275f83df14b43dc611b7b0
pemberontaktgr <vickyokaw@gmail.com>
2020-12-26 21:09:06 +0700

    aplikasi pertama

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  "editor.codeActionsOnSave": {
+    "source.fixAll": true,
+    "source.fixAll.eslint": false
+}
+};
---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, View } from "react-native"
+
+const App = () => {
+  return (
+    <View>
+      <View style={{ width: 80, height: 80, backgroundcolor: '#00BCD4' }} />
+      <Text>Vicky </Text>
+      <Text>Programmer </Text>
+      <Text>Oka</Text>
+      <Text>Widi</Text>
+    </View>
+  );
+};
+
+const Programmer = () => {
+  return (<Text>Oka Widiantara</Text>
+  )
+};
+
+export default App;
